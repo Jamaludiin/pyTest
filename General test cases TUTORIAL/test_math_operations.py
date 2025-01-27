7a3d02900d215759d8a4f7d021c657e651a4477b
jamal <jamalkacade@gmail.com>
2025-01-27 17:14:56 +0800

    testing math file using parametrized test

--- General test cases TUTORIAL/test_math_operations.py
+++ General test cases TUTORIAL/test_math_operations.py
@@ -1,7 +1,10 @@
 # test_math_operations.py
 
-from Simple_Projects.math_operations import add, subtract
+import pytest
 
+from Simple_Projects.math_operations import add, subtract, multipilication
+
+# pytest test_math_operations.py::test_multipilication
 def test_add():
     assert add(3, 2) == 5
     assert add(-1, 1) == 0
@@ -9,3 +12,14 @@ def test_add():
 def test_subtract():
     assert subtract(3, 2) == 1
     assert subtract(5, 5) == 0
+
+
+@pytest.mark.parametrize("x, y, result", [
+
+    (2, 2, 4),
+    (2, 3, 6),
+    (2, 4, 8),
+    ])
+
+def test_multipilication(x, y, result):
+    assert multipilication(x, y) == result
