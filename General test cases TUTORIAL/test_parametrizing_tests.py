32ee363c6a931bbe76db8f3c11cd9f7b8037b293
jamal <jamalkacade@gmail.com>
2025-01-27 16:14:30 +0800

    paramerizing tests

---
+++ General test cases TUTORIAL/test_parametrizing_tests.py
@@ -0,0 +1,24 @@
+# You can run the same test with multiple inputs using parametrize.
+
+# pytest test_parametrizing_tests.py
+import pytest
+
+@pytest.mark.parametrize("x, y, result", [
+    (2, 3, 5),  # Test case 1
+    (1, 1, 2),  # Test case 2
+    (0, 0, 0),  # Test case 3
+])
+
+
+def test_add(x, y, result):
+    assert x + y == result
+
+@pytest.mark.parametrize("x, y, result", [
+
+    (2, 2, 4),
+    (2, 3, 6),
+    (2, 4, 8),
+    ])
+
+def test_multiplication(x,y,result):
+    assert x * y == result
