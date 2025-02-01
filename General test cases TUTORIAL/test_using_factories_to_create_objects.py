029466b09098d688bc5cf78fba700ac1b90aea75
jamal <jamalkacade@gmail.com>
2025-02-01 13:15:59 +0800

    Using a factory function to generate test data dynamically

---
+++ General test cases TUTORIAL/test_using_factories_to_create_objects.py
@@ -0,0 +1,18 @@
+# Using a factory function to generate test data dynamically.
+
+# pytest -v test_using_factories_to_create_objects.py
+
+import pytest
+
+@pytest.fixture
+def user_factory():
+    def create_user(name, age):
+        return {"name": name, "age": age}
+    return create_user
+
+def test_user(user_factory):
+    user = user_factory("Alice", 30)
+    assert user["name"] == "Alice"
+    assert user["age"] == 30
+
+
